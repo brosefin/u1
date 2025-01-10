45049ab9e084942cfbe154231d8b69286cc5bc9f
brosefin <josefinjarphagen@gmail.com>
2025-01-10 19:52:56 +0100

    JS
    Pseudokod för funktionen getCityByName

--- website/index.js
+++ website/index.js
@@ -1,27 +1,49 @@
 // Recommended: All functions declared here
-function createAllCityBoxes() {
-
-    for (let city of cities) {
-
-        if (city.name) {
-            let boxDOM = document.createElement;
-            citiesDOM.appendChild(boxDOM);
-            
-        }
-    }
-}
 
 // Skapa lista över alla städer
 //createAllCityBoxes
 
+function createAllCityBoxes() {
+    const citiesDOM = document.getElementById("cities");
+    console.log("Function createAllCityBoxes is called");
+
+    for (let city of cities) {
+
+        if (city.name) {
+            let boxDOM = document.createElement("div");
+            citiesDOM.appendChild(boxDOM);
+            boxDOM.textContent = city.name;
+            boxDOM.className = "cityBox";
+        }
+    }
+}
+
+createAllCityBoxes();
 
 //markCityBox (cityObject, kindOfCity)
+
+// function markCityBox (cityObject, kindOfCity){
+//  if targetCityName == target -> ge klassen .target
+ 
+// }
+
+//getCityByName
+// function getCityByName (targetCityName) {
+//     '';
+    
+//     for loop through cities array
+//         if
+//             true -> update h2, push city.name?
+//             false -> "Not found"
+// }
+//
+
 
 // Recommended: constants with references to existing HTML-elements
 const main = document.querySelector("main");
 const section = document.getElementById("links");
 const h2 = document.querySelector("h2");
-const citiesDOM = document.getElementById("cities");
+
 
 // Recommended: Ask for the city name and then the rest of the code
 // const targetCityName = prompt("Enter city");
